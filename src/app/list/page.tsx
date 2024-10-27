4784dd5655c298bfaaa52fa6e02c24880e4262ca
heeyeon9578 <heeyeon9578@naver.com>
2024-10-27 16:18:35 +0900

    /list 페이지 다이나믹 렌더링으로 변경

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -2,6 +2,8 @@ import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 import Post from "../../../src/util/types";
 
+export const dynamic = "force-dynamic";
+
 export default async function List() {
   // 데이터베이스 연결 및 데이터 가져오기
   const db = (await connectDB).db("forum");
